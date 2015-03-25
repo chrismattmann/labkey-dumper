e80eafad89e2a10ab9b1046aa3dfac9b90627b35
Chris Mattmann <mattmann@apache.org>
2015-03-24 23:44:30 -0400

    Initial commit of Labkey Study JSON dumper.

---
+++ README.md
@@ -0,0 +1,46 @@
+# Labkey Dumper
+
+This project builds upon the [Labkey Client](https://github.com/chrismattmann/labkey-client) program to connect to a Labkey instance and then to dump out the Study objects from it into a JSON format. The JSON format looks like:
+
+```
+{"studies":[
+   {
+     "ParticipantAliasSourceProperty" : "null",
+     "ParticipantAliasProperty" : "null",
+     "Description" : "Some Description",
+     "EndDate" : "null",
+     "_labkeyurl_Label" : "/labkey/project/Project Name/start.view?",
+     "AssayPlan" : "null",
+     "Grant" : "Some Foundation",
+     "StartDate" : "Tue Jan 01 00:00:00 EST 2008",
+     "Investigator" : "Some PI, PhD",
+     "ParticipantAliasDatasetId" : "null",
+     "Label" : "Interactive Example - Study",
+     "Species" : "null",
+     "Container" : "6f743c1b-9506-1032-b9bf-51af32c4d069",
+     "_labkeyurl_Container" : "/labkey/project/HIV-CD4%20Study/begin.view?",
+   }
+    ]
+}
+
+```
+
+# Building this appilcation
+
+0. mkdir -p $HOME/src && cd $HOME/src
+1. git clone https://github.com/chrismattmann/labkey-client.git
+2. cd labkey-client && mvn install assembly:assembly
+3. cd ..
+4. git clone https://github.com/chrismattmann/labkey-dumper.git
+5. cd labkey-dumper && mvn install assembly:assembly
+
+All done!
+
+# Running this application
+
+A sample command would be:
+
+```
+java -Djava.ext.dirs=target gov.nasa.jpl.celgene.labkey.LabkeyDumper --url labkey-url --user email--pass thepass --project "Default Project"
+
+```
---
+++ src/main/java/gov/nasa/jpl/celgene/labkey/LabkeyDumper.java
@@ -0,0 +1,103 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gov.nasa.jpl.celgene.labkey;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import org.labkey.remoteapi.CommandException;
+import org.labkey.remoteapi.Connection;
+import org.labkey.remoteapi.query.SelectRowsCommand;
+import org.labkey.remoteapi.query.SelectRowsResponse;
+
+public class LabkeyDumper {
+
+	private Connection connection = null;
+
+	private URL labkeyUrl;
+
+	public LabkeyDumper(URL labkeyUrl, String username, String password) {
+		this.connection = new Connection(labkeyUrl.toString(), username,
+				password);
+		this.labkeyUrl = labkeyUrl;
+
+	}
+
+	public void generateJSON(List<Map<String, Object>> studyData) {
+
+		// loop over the returned rows
+		System.out.println("{\"studies\":[");
+		for (Map<String, Object> study : studyData) {
+			System.out.println("{");
+			for (String key : study.keySet()) {
+				System.out.println("\"" + key + "\" : \"" + study.get(key)
+						+ "\",");
+			}
+			System.out.println("}");
+		}
+		System.out.println("]}");
+	}
+
+	public List<Map<String, Object>> dumpStudies(String projectName) throws IOException,
+			CommandException {
+		// create a SelectRowsCommand to call the selectRows.api
+		SelectRowsCommand cmd = new SelectRowsCommand("study", "Study");
+
+		// execute the command against the connection
+		// within the Api Test project folder
+		SelectRowsResponse resp = cmd.execute(this.connection, projectName);
+		System.out.println(resp.getRowCount() + " rows were returned.");
+		return resp.getRows();
+	}
+
+	public static void main(String[] args) throws Exception {
+		String url = null;
+		String user = null;
+		String pass = null;
+		String projectName = null;
+
+		String usage = "java LabkeyDumper [--url <url>] [--user <user/email>] [--pass <pass>] [--project <Project Name>]\n";
+
+		for (int i = 0; i < args.length; i++) {
+			if (args[i].equals("--url")) {
+				url = args[++i];
+			} else if (args[i].equals("--user")) {
+				user = args[++i];
+			} else if (args[i].equals("--pass")) {
+				pass = args[++i];
+			}
+			else if(args[i].equals("--project")){
+				projectName = args[++i];
+			}
+		}
+
+		if (isEmpty(url) || isEmpty(user) || isEmpty(pass) || isEmpty(projectName)) {
+			System.err.println(usage);
+			System.exit(1);
+		}
+
+		LabkeyDumper dumper = new LabkeyDumper(new URL(url), user, pass);
+		dumper.generateJSON(dumper.dumpStudies(projectName));
+	}
+
+	private static boolean isEmpty(String string) {
+		return string == null || (string != null && string.equals(""));
+	}
+}
